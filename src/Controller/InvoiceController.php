c671cb9ecb18efdbd2a9b6dab8c7f540fc836a2c
Software06 <software06@imagar.com>
2023-05-08 17:56:00 +0200

    Createinvoice 1.0

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -10,6 +10,7 @@ use App\Entity\UserS;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Invoice;
 use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class InvoiceController extends AbstractController
 {
@@ -30,4 +31,26 @@ class InvoiceController extends AbstractController
             'invoices'=>$pagination
         ]);
     }
+
+    #[Route('/createInvoice/{userB}/{userS}/{money}/{description}', name: 'createInvoice')]
+public function createInvoice($userB, $userS, $money, $description, Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator, ManagerRegistry $doctrine): Response
+{
+
+    $invoice = new Invoice();
+
+    $invoice->setUserB($userB);
+    $invoice->setUserS($userS); // Corregido: setUserS en lugar de setUserB
+    $invoice->setMoney($money); // Corregido: setMoney en lugar de setUserB
+    $invoice->setDescription($description); // Corregido: setDescription en lugar de setUserB
+    $invoice->setCreationDate(new DateTime());
+
+    $entityManager = $doctrine->getManager(); // Corregido: se eliminó "$this->"
+    $entityManager->persist($invoice); // Corregido: "$product" cambiado a "$invoice"
+    $entityManager->flush();
+
+    return $this->redirectToRoute('facturas');
+
+}
+
+
 }
